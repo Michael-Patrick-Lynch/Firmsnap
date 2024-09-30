7b6a8dd3c5ba1f90758f3a6485a1ff281fcce45c
Michael Lynch <mplynch2020@gmail.com>
2024-09-30 17:43:26 +0100

    Added login screen that takes the user to their feed when they login

--- firmsnap/settings.py
+++ firmsnap/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,7 +60,7 @@ ROOT_URLCONF = "firmsnap.urls"
 TEMPLATES = [
 	{
 		"BACKEND": "django.template.backends.django.DjangoTemplates",
-		"DIRS": [],
+		"DIRS": [os.path.join(BASE_DIR, 'templates')],
 		"APP_DIRS": True,
 		"OPTIONS": {
 			"context_processors": [
@@ -128,3 +129,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 TAILWIND_APP_NAME = 'theme'
+
+AUTH_USER_MODEL = 'app.User'
--- firmsnap/urls.py
+++ firmsnap/urls.py
@@ -16,9 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from app.views import CustomLoginView
 
 urlpatterns = [
     path("app/", include("app.urls")),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")), # django_tailwind
+
+    # Need a custom login view because I want to redirect users to 
+    # their feed when they login (as opposed to their profile)
+    path('accounts/login/', CustomLoginView.as_view(), name='login'),
+
+    # This provides the following URL patterns:
+    # accounts/login/ [name='login']
+    # accounts/logout/ [name='logout']
+    # accounts/password_change/ [name='password_change']
+    # accounts/password_change/done/ [name='password_change_done']
+    # accounts/password_reset/ [name='password_reset']
+    # accounts/password_reset/done/ [name='password_reset_done']
+    # accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+    # accounts/reset/done/ [name='password_reset_complete']
+    path("accounts/", include("django.contrib.auth.urls")),
+
+    
 ]
