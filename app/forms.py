830da4dc046b15a626cb784d9987cb56ed4f2837
Michael Lynch <mplynch2020@gmail.com>
2024-10-02 23:24:51 +0100

    Added ability to leave comments

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from .models import Post
-
+from .models import Post, Comment
 
 class PostCreationForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['post_title', 'post_text']
+
+class CommentCreationForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment_text']
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import logging
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
-from app.forms import PostCreationForm
+from app.forms import PostCreationForm, CommentCreationForm
 from .models import Post, Comment
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.views import LoginView
@@ -17,9 +17,21 @@ def post_detail(request, post_id):
 
     # You can only view posts from your own organisation
     if user.is_authenticated and user.organisation_id == post_obj.organisation_id:
+        if request.method == 'POST':
+            comment_form = CommentCreationForm(request.POST)
+            if comment_form.is_valid():
+                new_comment = comment_form.save(commit=False)
+                new_comment.user_id = request.user
+                new_comment.post_id = Post.objects.get(pk=post_id)
+
+                new_comment.save()
+                return redirect('post_detail', post_id=post_id)
+        else:
+            comment_form = CommentCreationForm()
+
         list_of_comments = Comment.objects.filter(post_id=post_id)
         template = loader.get_template("app/post_detail.html")
-        context = {"post_obj" : post_obj, "list_of_comments": list_of_comments}
+        context = {"post_obj" : post_obj, "list_of_comments": list_of_comments,'comment_form': comment_form, 'post_id':post_id}
         return HttpResponse(template.render(context, request))
     else:
         return redirect('login')
